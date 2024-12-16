f9178eece427d096b0eb799b0e2f06203306092f
Santiago Squarzon <squarzon.santiago@gmail.com>
2024-12-16 11:50:31 -0300

    initial commit

---
+++ src/PowerShellKusto/ConnectKustoCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Management.Automation;
+
+namespace PowerShellKusto;
+
+[Cmdlet(VerbsCommunications.Connect, "Kusto")]
+public sealed class ConnectKustoCommand : PSCmdlet
+{
+    [Parameter(Mandatory = true, Position = 0)]
+    public string Cluster { get; set; } = null!;
+
+    [Parameter(Mandatory = true, Position = 1)]
+    public string Database { get; set; } = null!;
+
+    [Parameter(Mandatory = true, ParameterSetName = "Credential")]
+    [Credential]
+    public PSCredential Credential { get; set; } = null!;
+
+    [Parameter(Mandatory = true, ParameterSetName = "Credential")]
+    public Guid TenantId { get; set; }
+
+    [Parameter(ParameterSetName = "Identity")]
+    public SwitchParameter Identity { get; set; }
+
+    protected override void EndProcessing()
+    {
+
+    }
+}
